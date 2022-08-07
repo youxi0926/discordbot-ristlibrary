b4f4f3247cbb177187ed3348fef2e21a23113e9c
youxi0926 <107108905+youxi0926@users.noreply.github.com>
2022-08-08 02:30:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃーん')    
 
 
 token = getenv('DISCORD_BOT_TOKEN')
