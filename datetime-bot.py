3583fe1d3dd6a7571e294f9972d7c623744f8536
youxi0926 <107108905+youxi0926@users.noreply.github.com>
2022-08-24 14:34:20 +0900

    Create datetime-bot.py

---
+++ datetime-bot.py
@@ -0,0 +1,23 @@
+#coding:UTF-8
+import discord
+from discord.ext import tasks
+from datetime import datetime 
+
+TOKEN = "**********" #トークン
+CHANNEL_ID = ********** #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '00:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('0:00だよ')  
+
+#ループ処理実行
+loop.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
