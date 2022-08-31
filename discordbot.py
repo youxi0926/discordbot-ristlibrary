934ef4e18a81340f48c7cdfaa136cb2fd17402b4
youxi0926 <107108905+youxi0926@users.noreply.github.com>
2022-08-31 12:06:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,4 +23,29 @@ async def neko(ctx):
 
 
 token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+bot.run(from discord.ext import commands
+from os import getenv
+import traceback
+
+bot = commands.Bot(command_prefix='/')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+    
+
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃーん')    
+
+
+token = getenv('DISCORD_BOT_TOKEN')
+bot.run(MTAwNDgxODIxNDYzNjc1Mjk1Ng.GWd8WO.N5DwwQISQnRgU94bYVq2JICbOx9ml9PfgChCbk)
