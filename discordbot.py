244c6405e9c97615859760699711504a1c4e3709
youxi <is0661fi@ed.ritsumei.ac.jp>
2022-09-09 20:26:31 +0900

    bug fix -20:26

--- discordbot.py
+++ discordbot.py
@@ -3,18 +3,16 @@ import traceback
 import discord
 from discord.ext import tasks
 from datetime import datetime
-from os import getenv 
+from os import getenv
 
 bot = commands.Bot(command_prefix='/')
 client = discord.Client()
-
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
 
 @bot.command()
 async def ping(ctx):
@@ -27,12 +25,9 @@ CHANNEL_ID = 998859031076675587
 
 @tasks.loop(seconds=60)
 async def loop():
+    await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
-    await channel.send('毎分投稿するぜー！')
-    now = datetime.now().strftime('%H:%M')
-
-    if now == '00:00':
-        await channel.send('0:00だよ')
+    await channel.send('時間だよ')
 loop.start()
 
 token = getenv('DISCORD_BOT_TOKEN')
