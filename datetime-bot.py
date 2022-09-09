ebaa03e37a085aa870a0758aa10e0d5b8881500e
youxi0926 <107108905+youxi0926@users.noreply.github.com>
2022-09-09 18:33:32 +0900

    Update datetime-bot.py

--- datetime-bot.py
+++ datetime-bot.py
@@ -1,15 +1,1 @@
-import discord
-from discord.ext import tasks
-from datetime import datetime 
 
-TOKEN = "**********" #トークン
-CHANNEL_ID = "**********" #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 60秒に一回ループ
-
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
