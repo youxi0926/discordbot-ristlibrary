7d76f2bfe3e277211e73f040124c26bee5ce0fb5
youxi <is0661fi@ed.ritsumei.ac.jp>
2022-09-09 20:01:41 +0900

    update : loop

--- datetimebot.py
+++ datetimebot.py
@@ -2,17 +2,19 @@ import discord
 from discord.ext import tasks
 from datetime import datetime 
 
-TOKEN = "**********" #トークン
-CHANNEL_ID = ********** #チャンネルID
+TOKEN = getenv('DISCORD_BOT_TOKEN) #トークン
+CHANNEL_ID = 998859031076675587 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 # 60秒に一回ループ
 @tasks.loop(seconds=60)
 async def loop():
     # 現在の時刻
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send("毎分投稿するぜー！")
     now = datetime.now().strftime('%H:%M')
+
     if now == '00:00':
-        channel = client.get_channel(CHANNEL_ID)
         await channel.send('0:00だよ')  
 #ループ処理実行
 loop.start()
