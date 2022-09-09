56dd523d9abebba2f1dceed77af1ee75fce65bea
youxi <is0661fi@ed.ritsumei.ac.jp>
2022-09-09 20:08:57 +0900

    bug:fixx

--- datetimebot.py
+++ datetimebot.py
@@ -3,21 +3,16 @@ from discord.ext import tasks
 from datetime import datetime
 from os import getenv 
 
-TOKEN = getenv('DISCORD_BOT_TOKEN') #トークン
-CHANNEL_ID = 998859031076675587 #チャンネルID
-# 接続に必要なオブジェクトを生成
+TOKEN = getenv('DISCORD_BOT_TOKEN')
+CHANNEL_ID = 998859031076675587
 client = discord.Client()
-# 60秒に一回ループ
 @tasks.loop(seconds=60)
 async def loop():
-    # 現在の時刻
     channel = client.get_channel(CHANNEL_ID)
     await channel.send('毎分投稿するぜー！')
     now = datetime.now().strftime('%H:%M')
 
     if now == '00:00':
-        await channel.send('0:00だよ')  
-#ループ処理実行
+        await channel.send('0:00だよ')
 loop.start()
-# Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
