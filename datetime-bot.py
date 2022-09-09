47dd7e3e714b5f70d5b55cfdf78c7cbd53cb3415
youxi0926 <107108905+youxi0926@users.noreply.github.com>
2022-09-09 18:31:58 +0900

    Update datetime-bot.py

--- datetime-bot.py
+++ datetime-bot.py
@@ -8,13 +8,6 @@ CHANNEL_ID = "**********" #チャンネルID
 client = discord.Client()
 
 # 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '00:00':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('0:00だよ')  
 
 #ループ処理実行
 loop.start()
